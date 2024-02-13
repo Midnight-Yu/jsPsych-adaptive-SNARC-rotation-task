97ea4f0c8fe9c0e82b8c918304dd57b226644b6d
Midnight-Yu <156907600+Midnight-Yu@users.noreply.github.com>
2024-02-13 20:29:51 +0800

    Simplify exp.js

--- scripts/exp.js
+++ scripts/exp.js
@@ -1,68 +1,68 @@
 let jsPsych = initJsPsych({
-    on_finish: function() {
+    on_finish: function () {
         jsPsych.data
             .get()
-            .localSave('csv','data.csv')
-            }
+            .localSave('csv', 'data.csv')
+    }
 }); //初始化jsPsych
 
 let instruction = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <div class='experiment-instruction'> 
     <p>这是一个奇偶判断实验</p>
     <p>当你看到屏幕上的数字后，请按键反应</p>
     <p>如果数字是奇数（单数），请按 F 键</p>
     <p>如果数字是偶数（双数），请按 J 键</p>
-    </div>
     `,
-    post_trial_gap: 500
+    post_trial_gap: 500,
+    css_classes: "experiment-instruction"
 } //定义实验用的div，方便操控所有CSS
 
 let trials = {
     type: jsPsychHtmlKeyboardResponse,
-    save_trial_parameters:{
-        trial_duration: true, 
+    css_classes: "experiment-content",
+    save_trial_parameters: {
+        trial_duration: true,
         choices: true,
         post_trial_gap: true
     },
     data: {
         category: jsPsych.timelineVariable('category')
     },
-    timeline:[
+    timeline: [
         {  //空屏
-            stimulus: jsPsych.timelineVariable('content'), 
+            stimulus: jsPsych.timelineVariable('content'),
             choices: 'NO_KEYS',
             trial_duration: 1000
         },
         { //注视点
-            stimulus: "<div class='experiment-content'>+</div>", 
-             choices: 'NO_KEYS', 
-            trial_duration: 500, 
-            post_trial_gap: function() {
-                random_interval = 400 + 200*Math.random();
+            stimulus: "+",
+            choices: 'NO_KEYS',
+            trial_duration: 500,
+            post_trial_gap: function () {
+                random_interval = 400 + 200 * Math.random();
                 return random_interval;
             }
         },
         {  //刺激
-            stimulus: jsPsych.timelineVariable('content'), 
-            choices: ["f","j"],
+            stimulus: jsPsych.timelineVariable('content'),
+            choices: ["f", "j"],
             trial_duration: 150
         },
         {  //空屏
-            stimulus: jsPsych.timelineVariable('content'), 
-            choices: ["f","j"]
+            stimulus: jsPsych.timelineVariable('content'),
+            choices: ["f", "j"]
         }
     ],
-    timeline_variables:[ 
-        { content: "<div class='experiment-content'>1</div>", category: 1},
-        { content: "<div class='experiment-content'>2</div>", category: 2},
-        { content: "<div class='experiment-content'>3</div>", category: 1},
-        { content: "<div class='experiment-content'>4</div>", category: 2},
-        { content: "<div class='experiment-content'>5</div>", category: 1},
-        { content: "<div class='experiment-content'>6</div>", category: 2}
+    timeline_variables: [
+        { content: "1", category: 1 },
+        { content: "2", category: 2 },
+        { content: "3", category: 1 },
+        { content: "4", category: 2 },
+        { content: "5", category: 1 },
+        { content: "6", category: 2 }
     ],
-    sample:{
+    sample: {
         type: 'fixed-repetitions',
         size: 2
     },
@@ -80,12 +80,13 @@ let trials = {
 let ending = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <div class='experiment-instruction'><p>实验已结束</p></div>
+    <p>实验已结束</p>
     `,
-    post_trial_gap: 500
+    post_trial_gap: 500,
+    css_classes: "experiment-instruction"
 }
 
 jsPsych.run([
-    instruction,trials,ending
+    instruction, trials, ending
 ])
 
