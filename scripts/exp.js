512f893fe4af0da72ea9a8970b77e3b8d571f217
Midnight-Yu <156907600+Midnight-Yu@users.noreply.github.com>
2024-03-27 21:54:29 +0800

    Update exp.js

--- scripts/exp.js
+++ scripts/exp.js
@@ -32,8 +32,8 @@ resultList = simpleList.concat(simpleList);
 let parity_trial_times = 0; //休息试次检测变量
 let rotation_trial_times = 0;
 
-let rotation_value = 5; //旋转试次自适应需要的变量
-let standard_rt = 400; //先随便写一个
+let rotation_value = 4; //旋转试次自适应需要的变量
+let standard_rt = 700; //先随便写一个
 let rotation_rt_list = [];
 
 let parity_practice_corr_count = 0; //练习试次检测正确率的变量
@@ -120,7 +120,7 @@ let instruction = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
     <p>欢迎加入测验！</p>
-    <p>本测验分为两个阶段，两个阶段有不同的任务</p>
+    <p>本测验分为三个阶段，三个阶段有不同的任务</p>
     <p>每个阶段开始前，都有练习流程帮助你熟悉测验</p>
     <p></p>
     <p>准备好后，按空格键进入测验</p>
@@ -187,6 +187,33 @@ let instruction_parity = {
     ]
 };
 
+let instruction_parity_2 = {
+    type: jsPsychHtmlKeyboardResponse,
+    timeline: [
+        {   // 第一页
+            stimulus: `
+            <div class='experiment-instruction'>
+            <h2>第二部分</h2>
+
+            <p>这个部分的规则如下<br><br>
+            当你看到单数时，请按下<span style="color:green"> J </span>键<br>
+            当你看到双数时，请按下<span style="color:green"> F </span>键</p>
+
+            <p>例如，当你看到 3 时，请按下 J 键<br>
+            例如，当你看到 6 时，请按下 F 键</p>
+
+            <p>注意，请不要用同一只手按键<br>
+            请将左手放在 F 键上，右手放在 J 键上</p>
+
+            <p>按空格键进入练习</p>
+            </div>
+            `,
+            choices: [' '],
+            post_trial_gap: 1000
+        }
+    ]
+};
+
 let parity_training = {
     type: jsPsychHtmlKeyboardResponse,
     css_classes: "experiment-content",
@@ -196,7 +223,8 @@ let parity_training = {
         post_trial_gap: true
     },
     data: {
-        parity: jsPsych.timelineVariable('parity')
+        parity: jsPsych.timelineVariable('parity'),
+        parity_type: 1
     },
     timeline: [
         {  //空屏
@@ -305,6 +333,125 @@ let parity_training = {
     }
 };
 
+let parity_training_2 = {
+    type: jsPsychHtmlKeyboardResponse,
+    css_classes: "experiment-content",
+    save_trial_parameters: {
+        trial_duration: true,
+        choices: true,
+        post_trial_gap: true
+    },
+    data: {
+        parity: jsPsych.timelineVariable('parity'),
+        parity_type: 2
+    },
+    timeline: [
+        {  //空屏
+            stimulus: " ",
+            choices: 'NO_KEYS',
+            trial_duration: 1000
+        },
+        { //注视点
+            stimulus: "+",
+            choices: 'NO_KEYS',
+            trial_duration: 500,
+            post_trial_gap: function () {
+                let random_interval = 400 + Math.floor(200 * Math.random());
+                return random_interval;
+            }
+        },
+        {  //刺激
+            stimulus: jsPsych.timelineVariable('content'),
+            choices: ["f", "j"],
+            //stimulus_duration: 150
+            on_finish: function (data) {
+                data.is_trial = false;
+                data.trial_class = 'parity';
+                //赋予正确答案值
+                if (data.parity === 1) {
+                    data.correct = (data.response === 'j');
+                }
+                else {
+                    data.correct = (data.response === 'f');
+                }
+
+                if (data.correct) {
+                    parity_practice_corr_count++
+                }
+            }
+        },
+        { //反馈，注意只有练习阶段有反馈
+            stimulus: function () {
+                let last_trial_correct = jsPsych.data.get().last(1).values()[0].correct;
+                if (last_trial_correct) {
+                    return "<p><span style='color:green; font-size:20%'>正确</span></p>";
+                } else {
+                    return "<p><span style='color:red; font-size:20%'>错误</span></p>";
+                };
+            },
+            choices: 'NO_KEYS',
+            trial_duration: 1000,
+        }
+
+    ],
+    timeline_variables: [
+        { content: "1", parity: 1 },
+        { content: "2", parity: 2 },
+        { content: "3", parity: 1 },
+        { content: "4", parity: 2 },
+        { content: "5", parity: 1 },
+        { content: "6", parity: 2 }
+    ],
+    sample: {
+        type: 'custom',
+        fn: function (t) { // 抄书上的
+            let new_t = t.concat(t); //把目标数组复制两份，拼到一起
+
+            let loop_state = true; // 初始化循环状态
+
+            while (loop_state) {
+                loop_state = false;
+
+                for (let i = 0; i < new_t.length; i++) {
+                    let rand_index = Math.floor(Math.random() * (new_t.length - i) + i); 
+                    [new_t[i], new_t[rand_index]] = [new_t[rand_index], new_t[i]]; // 交换索引
+                }
+
+                let repeat = 1;
+
+                for (let i = 1; i < new_t.length; i++) {
+                    let parity = parity_training.timeline_variables[new_t[i]].parity;
+                    let last_parity = parity_training.timeline_variables[new_t[i - 1]].parity;
+
+                    if (parity === last_parity) {
+                        repeat++;
+                    }
+                    else {
+                        repeat = 1;
+                    }
+
+                    if (repeat >= 4) {
+                        loop_state = true;
+                        break;
+                    }
+                }
+            }
+
+            sampled_t = new_t.slice(0,10)
+            return sampled_t;
+        }
+    },
+    loop_function: function (data) {
+        if (parity_practice_corr_count >= 7) {
+            return false;
+        }
+        else {
+            parity_practice_corr_count = 0;
+            return true;
+        }
+    }
+};
+
 let parity_trials = {
     type: jsPsychHtmlKeyboardResponse,
     css_classes: "experiment-content",
@@ -314,7 +461,8 @@ let parity_trials = {
         post_trial_gap: true
     },
     data: {
-        parity: jsPsych.timelineVariable('parity')
+        parity: jsPsych.timelineVariable('parity'),
+        parity_type: 1
     },
     timeline: [
         {  //空屏
@@ -394,8 +542,202 @@ let parity_trials = {
                 return index % 2 != 0;
             });
 
-            const original_odd_pool = [...Array(20)].flatMap(() => odd_list);
-            const original_even_pool = [...Array(20)].flatMap(() => even_list);
+            const original_odd_pool = [...Array(10)].flatMap(() => odd_list);
+            const original_even_pool = [...Array(10)].flatMap(() => even_list);
+
+            let new_t = [];
+            let oddCount = 0;
+            let evenCount = 0;
+            let success = false;
+            let index = -1;
+            let odd_pool = [...original_odd_pool]
+            let even_pool = [...original_even_pool];
+
+            while (success != true) {
+                let currentElement;
+
+                // 取元素语句
+                if (odd_pool.length === 0) {
+                    if (even_pool.length === 0) {
+                        // 生成成功，停止生成，结束循环
+                        success = true;
+                    } else {
+                        if (evenCount === 3) {
+                            // 双数连续判断为真，生成失败，重新初始化
+                            new_t = [];
+                            oddCount = 0;
+                            evenCount = 0;
+                            index = -1;
+                            odd_pool = [...original_odd_pool]
+                            even_pool = [...original_even_pool];
+                            continue;
+                        } else {
+                            // 随机取一个双数，跳到判断
+                            currentElement = even_pool[Math.floor(Math.random() * even_pool.length)];
+                            index = even_pool.indexOf(currentElement);
+                            if (index > -1) {
+                                even_pool.splice(index, 1);
+                            }
+                        }
+                    }
+                } else if (even_pool.length === 0) {
+                    if (oddCount === 3) {
+                        // 单数连续判断为真，生成失败，重新初始化
+                        new_t = [];
+                        oddCount = 0;
+                        evenCount = 0;
+                        index = -1;
+                        odd_pool = [...original_odd_pool]
+                        even_pool = [...original_even_pool];
+                        continue;
+                    } else {
+                        // 随机取一个单数，跳到判断
+                        currentElement = odd_pool[Math.floor(Math.random() * odd_pool.length)];
+                        index = odd_pool.indexOf(currentElement);
+                        if (index > -1) {
+                            odd_pool.splice(index, 1);
+                        }
+                    }
+                } else {
+                    if (oddCount === 3) {
+                        // 单数连续判断为真，取双数
+                        currentElement = even_pool[Math.floor(Math.random() * even_pool.length)];
+                        index = even_pool.indexOf(currentElement);
+                        if (index > -1) {
+                            even_pool.splice(index, 1);
+                        }
+                    } else if (evenCount === 3) {
+                        // 双数连续判断为真，取单数
+                        currentElement = odd_pool[Math.floor(Math.random() * odd_pool.length)];
+                        index = odd_pool.indexOf(currentElement);
+                        if (index > -1) {
+                            odd_pool.splice(index, 1);
+                        }
+                    } else {
+                        // 随机选择一个池子，随机从中取一个数
+                        const pool = Math.random() < 0.5 ? odd_pool : even_pool;
+                        currentElement = pool[Math.floor(Math.random() * pool.length)];
+                        index = pool.indexOf(currentElement);
+                        if (index > -1) {
+                            pool.splice(index, 1);
+                        }
+                    }
+                }
+
+                // 判断语句
+                if (currentElement % 2 === 0) {
+                    // 上面取的是双数，双数++，单数=0
+                    evenCount++;
+                    oddCount = 0;
+                } else {
+                    // 上面取的是单数，单数++，双数=0
+                    oddCount++;
+                    evenCount = 0;
+                }
+
+                // 将取到的元素添加到结果数组
+                if (currentElement != undefined) {
+                    new_t.push(currentElement);
+                }
+            }
+
+            return new_t;
+        }
+    }
+}
+
+let parity_trials_2 = {
+    type: jsPsychHtmlKeyboardResponse,
+    css_classes: "experiment-content",
+    save_trial_parameters: {
+        trial_duration: true,
+        choices: true,
+        post_trial_gap: true
+    },
+    data: {
+        parity: jsPsych.timelineVariable('parity'),
+        parity_type: 2
+    },
+    timeline: [
+        {  //空屏
+            stimulus: " ",
+            choices: 'NO_KEYS',
+            trial_duration: 1000
+        },
+        { //注视点
+            stimulus: "+",
+            choices: 'NO_KEYS',
+            trial_duration: 500,
+            post_trial_gap: function () {
+                let random_interval = 400 + Math.floor(200 * Math.random());
+                return random_interval;
+            }
+        },
+        {  //刺激
+            stimulus: jsPsych.timelineVariable('content'),
+            choices: ["f", "j"],
+            //stimulus_duration: 150
+            on_finish: function (data) {
+                parity_trial_times++
+
+                data.is_trial = true;
+                data.trial_class = 'parity';
+                //赋予正确答案值
+                if (data.parity === 1) {
+                    data.correct = (data.response === 'j');
+                }
+                else {
+                    data.correct = (data.response === 'f');
+                }
+            }
+        },
+        {  //休息试次
+            css_classes: "experiment-instruction",
+            timeline: [
+                {
+                    stimulus: `请休息片刻，稍后测验将继续`,
+                    trial_duration: 30000,
+                    choices: 'NO_KEYS'
+                },
+                {
+                    stimulus: `休息已终止，按空格可继续测验`,
+                    choices: ' '
+                },
+            ],
+            conditional_function: function () {
+                if (parity_trial_times % 24 === 0) {
+                    return true
+                }
+                else {
+                    return false
+                }
+            },
+        }
+    ],
+    timeline_variables: [
+        { content: "1", parity: 1 },
+        { content: "2", parity: 2 },
+        { content: "3", parity: 1 },
+        { content: "4", parity: 2 },
+        { content: "5", parity: 1 },
+        { content: "6", parity: 2 }
+    ],
+    sample: {
+        type: 'custom',
+        fn: function (t) {
+            // 生成式伪随机序列算法
+            // 思想：将需要放进数组的元素按照需要的分类分成两个池子，随后从左到右依次生成这个数组。
+            // 如果两个池子的元素都符合条件，则随机选择一个；如果只有一个盒子满足条件，则只选择其中一个。
+
+            const odd_list = t.filter(function(element, index) {
+                return index % 2 == 0;
+            });
+            const even_list = t.filter(function(element, index) {
+                return index % 2 != 0;
+            });
+
+            const original_odd_pool = [...Array(10)].flatMap(() => odd_list);
+            const original_even_pool = [...Array(10)].flatMap(() => even_list);
 
             let new_t = [];
             let oddCount = 0;
@@ -504,7 +846,7 @@ let instruction_rotation = {
         {   // 第一页
             stimulus: `
             <div class='experiment-instruction'>
-            <h2>第二部分</h2>
+            <h2>第三部分</h2>
 
             <p>这个部分的规则如下<br><br>
             当你看到图形向左旋转时，请按下<span style="color:cyan"> F </span>键<br>
@@ -882,6 +1224,7 @@ jsPsych.run([
     welcome, data_collect, instruction_fullscreen, browser_check,
     instruction,
     instruction_parity, practice_instruction, parity_training, practice_feedback, parity_trials, 
+    instruction_parity_2, practice_instruction, parity_training_2, practice_feedback, parity_trials_2, 
     instruction_rotation, practice_instruction, rotation_training, practice_feedback, rotation_trials, 
     ending
 ])
